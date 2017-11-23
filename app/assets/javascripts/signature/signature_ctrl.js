22f5d5154b44dc0868ed1c22d4504ebc80174ae6
Kurt Metulj <kurt.metulj@gmail.com>
2017-11-23 22:39:04 +0100

    Drag and Drop on multiple rows and columns

--- app/assets/javascripts/signature/signature_ctrl.js
+++ app/assets/javascripts/signature/signature_ctrl.js
@@ -6,19 +6,49 @@ angular
 SignatureCtrl.$inject = ['$scope'];
 
 function SignatureCtrl($scope) {
-    $scope.models = {
-        selected: null,
-        lists: {"A": [], "B": []}
-    };
 
-    // Generate initial model
-    for (var i = 1; i <= 3; ++i) {
-        $scope.models.lists.A.push({label: "Item A" + i});
-        $scope.models.lists.B.push({label: "Item B" + i});
+    // Initialize model
+    $scope.model = [[], []];
+    var id = 10;
+
+    initialize();
+
+    function initialize() {
+        angular.forEach(['all', 'move', 'copy', 'link', 'copyLink', 'copyMove'], function(effect, i) {
+            var container = {items: [], effectAllowed: effect};
+            for (var k = 0; k < 7; ++k) {
+                container.items.push({label: effect + ' ' + id++, effectAllowed: effect});
+            }
+            $scope.model[i % $scope.model.length].push(container);
+        });
     }
 
-    // Model to JSON for demo purpose
-    $scope.$watch('models', function(model) {
+    $scope.dragoverCallback = function(index, external, type, callback) {
+        $scope.logListEvent('dragged over', index, external, type);
+        // Invoke callback to origin for container types.
+        if (type == 'container' && !external) {
+            console.log('Container being dragged contains ' + callback() + ' items');
+        }
+        return index < 10; // Disallow dropping in the third row.
+    };
+
+    $scope.dropCallback = function(index, item, external, type) {
+        $scope.logListEvent('dropped at', index, external, type);
+        // Return false here to cancel drop. Return true if you insert the item yourself.
+        return item;
+    };
+
+    $scope.logEvent = function(message) {
+        console.log(message);
+    };
+
+    $scope.logListEvent = function(action, index, external, type) {
+        var message = external ? 'External ' : '';
+        message += type + ' element was ' + action + ' position ' + index;
+        console.log(message);
+    };
+
+    $scope.$watch('model', function(model) {
         $scope.modelAsJson = angular.toJson(model, true);
     }, true);
 };
