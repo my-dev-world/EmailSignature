bbd25bbe860ca039acc1e2c812422e7228d7be5c
Kurt Metulj <kurt.metulj@gmail.com>
2017-11-28 18:20:44 +0100

    Update configuration for Heroku

--- config/application.rb
+++ config/application.rb
@@ -17,5 +17,7 @@ module EmailSignature
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.assets.path << Rails.root.join("vendor/assets/bower_components")
   end
 end
